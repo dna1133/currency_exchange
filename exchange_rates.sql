e89d18e24076eb89d22fc7d08496d48c8caf8eca
Alex Kor <dna1133@gmail.com>
2024-11-20 15:57:22 +0300

    sql: added exchange rates

---
+++ exchange_rates.sql
@@ -0,0 +1,4 @@
+INSERT INTO "ExchangeRates" (oid, base_currency_oid, target_currency_oid, rate) VALUES
+('2cf2c251-b9ba-4578-b85e-9826bee39452', '2cf9c251-b9ba-4528-b85e-9826bee39652', '50668fa3-d06c-48b5-9297-329a37907bb6', 1.1),
+('50668fa3-d06c-48b5-9217-329a37917bb6', '2cf9c251-b9ba-4528-b85e-9826bee39652', '2fde670f-5b69-46cf-bf42-7268c307ec9a', 7.24),
+('24668fa3-326c-48b5-5557-329a37907bb6', 'f29ebd84-9199-4044-b79c-701bbe08b766', '50668fa3-d06c-48b5-9297-329a37907bb6', 0.61)
