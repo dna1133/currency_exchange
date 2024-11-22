173d9217963c7dda5753b3b26009307df9b80f21
Alex Kor <dna1133@gmail.com>
2024-11-22 18:28:29 +0300

    feat: added sql execute

--- src/currency_exchange/gateways/database/alembic/versions/a5dd10b65d68_initial_migration.py
+++ src/currency_exchange/gateways/database/alembic/versions/a5dd10b65d68_initial_migration.py
@@ -25,7 +25,7 @@ def upgrade() -> None:
         "Currencies",
         sa.Column("oid", sa.String(), nullable=False),
         sa.Column("code", sa.String(), nullable=False),
-        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("fullname", sa.String(), nullable=False),
         sa.Column("sign", sa.String(), nullable=False),
         sa.PrimaryKeyConstraint("oid"),
     )
@@ -44,6 +44,29 @@ def upgrade() -> None:
             ["Currencies.oid"],
         ),
         sa.PrimaryKeyConstraint("oid"),
+    )
+    op.execute(
+        """
+        INSERT INTO "Currencies" (fullname, code, sign, oid) VALUES
+('US Dollar', 'USD', '$', '2cf9c251-b9ba-4528-b85e-9826bee39652'),
+('Euro', 'EUR', '€', '50668fa3-d06c-48b5-9297-329a37907bb6'),
+('Australian Dollar', 'AUD', 'A$', 'f29ebd84-9199-4044-b79c-701bbe08b766'),
+('Czech Koruna', 'CZK', 'Kč', '52c9d342-2726-4269-a941-74bd656b841b'),
+('Egyptian Pound', 'EGP', '£', 'bb3592eb-b802-459d-a86a-3ce2cf9d5e32'),
+('Russian Ruble', 'RUB', '₽', '409cb619-f65f-488b-a723-d0ead68a20aa'),
+('Indian Rupee', 'INR', '₹', 'fabbd277-89c9-4a33-aafa-6cb63747a822'),
+('Tenge', 'KZT', '₸', '0cf2790a-4e40-4d70-94bc-0e3e95cb4dfa'),
+('New Israeli Sheqel', 'ILS', '₪', '208668ca-647f-46ce-81f9-ced165c776cb'),
+('Mexican Peso', 'MXN', '$', '543169de-3276-4fcf-88c5-f073b249cd7c'),
+('Yuan Renminbi	', 'CNY', '¥', '2fde670f-5b69-46cf-bf42-7268c307ec9a')
+    """
+    )
+    op.execute(
+        """INSERT INTO "ExchangeRates" (oid, base_currency_oid, target_currency_oid, rate) VALUES
+('2cf2c251-b9ba-4578-b85e-9826bee39452', '2cf9c251-b9ba-4528-b85e-9826bee39652', '50668fa3-d06c-48b5-9297-329a37907bb6', 1.1),
+('50668fa3-d06c-48b5-9217-329a37917bb6', '2cf9c251-b9ba-4528-b85e-9826bee39652', '2fde670f-5b69-46cf-bf42-7268c307ec9a', 7.24),
+('24668fa3-326c-48b5-5557-329a37907bb6', 'f29ebd84-9199-4044-b79c-701bbe08b766', '50668fa3-d06c-48b5-9297-329a37907bb6', 0.61)
+    """
     )
     # ### end Alembic commands ###
 
