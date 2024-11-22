cf72e0236cbb35cbe0ddd5d4862d1b0fad342159
Alex Kor <dna1133@gmail.com>
2024-11-22 13:21:38 +0300

    feat: added redis cache

--- src/currency_exchange/api/routers/exchange.py
+++ src/currency_exchange/api/routers/exchange.py
@@ -1,7 +1,6 @@
 from decimal import Decimal
 import logging
-from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi_cache.decorator import cache
+from fastapi import APIRouter, Depends
 
 from currency_exchange.api.ischemas import ExchangePairArgs
 from currency_exchange.api.responses.exchange_resposes import (
@@ -11,9 +10,9 @@ from currency_exchange.api.responses.exchange_resposes import (
     patch_exchange_rate_responce,
     get_exchange_sum_responce,
 )
-from currency_exchange.domain.entity.exceptions import ApplicationError
-from currency_exchange.services.base_api_service import BaseService
-from currency_exchange.services.exchange_api_sevice import ExchangeService
+from currency_exchange.services.api_services.base_api_service import BaseService
+from currency_exchange.services.api_services.exchange_api_sevice import ExchangeService
+from currency_exchange.services.cache_services.redis_service import CacheService
 
 
 router = APIRouter(
@@ -23,17 +22,20 @@ router = APIRouter(
 
 log = logging.getLogger(__name__)
 
+cache = CacheService()
+
 
 @router.get("/", responses=get_all_exchange_rates_responce)
-@cache(expire=30)
+@cache.cached(expire=30)
 async def get_all_exchange_rates(
     service: BaseService = Depends(ExchangeService),
 ):
-    return await service.get_all_exchange_rates()
+    result = await service.get_all_exchange_rates()
+    return result
 
 
 @router.get("/{exchange_rate}", responses=get_exchange_rate_responce)
-@cache(expire=30)
+@cache.cached(expire=30)
 async def get_exchange_rate(
     exchange_rate: str,
     service: BaseService = Depends(ExchangeService),
@@ -42,7 +44,6 @@ async def get_exchange_rate(
 
 
 @router.post("/", responses=post_exchange_rate_responce)
-@cache(expire=30)
 async def post_exchange_rate(
     base_currency_code: str,
     target_currency_code: str,
@@ -55,7 +56,6 @@ async def post_exchange_rate(
 
 
 @router.patch("/{exchange_rate}", responses=patch_exchange_rate_responce)
-@cache(expire=30)
 async def patch_exchange_rate(
     exchange_rate: str, rate: Decimal, service: BaseService = Depends(ExchangeService)
 ):
@@ -63,7 +63,7 @@ async def patch_exchange_rate(
 
 
 @router.get("/exchange", responses=get_exchange_sum_responce)
-@cache(expire=30)
+@cache.cached(expire=30)
 async def get_exchange_sum(
     ext: ExchangePairArgs = Depends(), service: BaseService = Depends(ExchangeService)
 ):
---
+++ src/currency_exchange/services/cache_services/redis_service.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from decimal import Decimal
+from functools import wraps
+import json
+from typing import Any, Callable
+
+from redis.asyncio import Redis
+
+from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.orm import class_mapper
+
+from currency_exchange.core.conteiner import get_conteiner
+from currency_exchange.domain.entity.base import BaseEntity
+from currency_exchange.services.cache_services.base_cache_servise import (
+    BaseCacheService,
+)
+
+
+@dataclass
+class CacheService(BaseCacheService):
+    conteiner = get_conteiner()
+    redis = conteiner.resolve(Redis)
+
+    def cached(self, expire: int = 60):
+        def decorator(func: Callable):
+            @wraps(func)
+            async def wrapper(*args, **kwargs) -> Any:
+
+                cache_key = f"{func.__name__}:{args}:{kwargs}"
+                cached_result = await self.redis.get(cache_key)
+                if cached_result:
+                    return json.loads(cached_result)
+                result = await func(*args, **kwargs)
+                if isinstance(result, BaseEntity):
+                    result = result.to_dict()
+                elif isinstance(result, list):
+                    result = [self._sqlalchemy_to_dict(obj) for obj in result]
+                elif isinstance(result, object):
+                    result = self._sqlalchemy_to_dict(result)
+                elif isinstance(result, dict):
+                    result = self._sqlalchemy_to_dict(result)
+                await self.redis.set(cache_key, json.dumps(result), ex=expire)
+                return result
+
+            return wrapper
+
+        return decorator
+
+    def _sqlalchemy_to_dict(self, obj):
+        # print(
+        #     f"-------------------{type(obj)}, {isinstance(obj.__class__, DeclarativeMeta)}"
+        # )
+        if isinstance(obj.__class__, DeclarativeMeta):
+            result = {
+                c.key: getattr(obj, c.key) for c in class_mapper(obj.__class__).columns
+            }
+            result = self._sqlalchemy_to_dict(result)
+            return result
+        elif isinstance(obj, dict):
+            return {k: self._sqlalchemy_to_dict(v) for k, v in obj.items()}
+        elif isinstance(obj, Decimal):
+            return float(obj)
+        else:
+            return obj
