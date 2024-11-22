9323910873768ff8bd010c5b6218396501daf86c
Alex Kor <dna1133@gmail.com>
2024-11-22 18:14:37 +0300

    fix:  tables unique oid

--- src/currency_exchange/gateways/database/models/currencies.py
+++ src/currency_exchange/gateways/database/models/currencies.py
@@ -5,7 +5,7 @@ from currency_exchange.gateways.database.models.base import Base
 class Currencies(Base):
     __tablename__ = "Currencies"
 
-    oid = Column(String, primary_key=True)
+    oid = Column(String, primary_key=True, unique=True)
     code = Column(String, nullable=False, unique=True)
     fullname = Column(String, nullable=False)
     sign = Column(String, nullable=False)
--- src/currency_exchange/gateways/database/models/exchange_rates.py
+++ src/currency_exchange/gateways/database/models/exchange_rates.py
@@ -5,7 +5,7 @@ from currency_exchange.gateways.database.models.base import Base
 class ExchangeRates(Base):
     __tablename__ = "ExchangeRates"
 
-    oid = Column(String, primary_key=True)
+    oid = Column(String, primary_key=True, unique=True)
     base_currency_oid = Column("base_currency_oid", ForeignKey("Currencies.oid"))
     target_currency_oid = Column("target_currency_oid", ForeignKey("Currencies.oid"))
     rate = Column(DECIMAL, nullable=False)
